7bf8acd4cf3283d383c2fc1f430b349fba7d8ea3
System Administrator <root@oohatamisakonoMacBook-puro.local>
2017-11-26 11:50:40 +0900

    add Sec4-56

--- src/App.js
+++ src/App.js
@@ -3,12 +3,81 @@ import './App.css';
 import Person from './Person/Person'; 
 
 class App extends Component {
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age: '28' },
+      { id: 'vasdf1', name: 'Manu', age: '29' },
+      { id: 'asdf1', name: 'Stephanie', age: '26'}
+    ],
+    otherState: 'Some other value',
+    showPersons:false
+  }
+　 
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons});
+    
+
+    this.setState({
+      persons:[
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 28}
+      ]
+    })
+  }
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons.splice();
+
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
   render() {
+    const style= {
+      backgroundColor: 'white',
+      font: 'inherit', 
+      border: '1px solid blue', 
+      padding: '8px', 
+      cursor: 'pointer'
+    };
+    let persons = null; 
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              onClick={()=> this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}  
+              key={person.id}
+              changed={(event)=> this.nameChangeHandler(event, person.id)}
+            />
+          })}
+        </div>
+
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+         {persons}
       </div>
 
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,14 @@
 import React from 'react';
-const person = () => {
-  return <p>I'm a Person</p>
+import './Person.css';
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p>I'm {props.name} and I am {props.age}</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} />
+    </div>
+  )
+  //return <p>I'm a Person and I'm {Math.floor(Math.random() * 30)} years old!</p>
 }
 
 export default person;
