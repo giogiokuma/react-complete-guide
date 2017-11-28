03fe19b01d077a1274b3a8bf8c5e06a13788a0f5
Misako Ohata <misa@ohatadesign.com>
2017-11-28 21:32:32 +0900

    add sec5-64 media query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person'; 
+
 
 class App extends Component {
   state = {
@@ -40,19 +42,24 @@ class App extends Component {
     })
   }
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons.splice();
-
+    //const persons = this.state.persons.splice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
 
   render() {
     const style= {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font: 'inherit', 
       border: '1px solid blue', 
       padding: '8px', 
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color:'black'
+      }
     };
     let persons = null; 
 
@@ -71,18 +78,35 @@ class App extends Component {
         </div>
 
       );
+      style.backgroundColor = 'red';
+
+      style[':hover'] =  {
+        backgroundColor: 'lightred',
+        color: 'black'
+      };
     }
+
+    const classes = [''];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); 
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working</p>
+        <p className={classes.join('')}>This is really working</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
          {persons}
       </div>
-
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)' : {
+      width: '450px'
+    }
+  }
   return (
     <div className="Person">
       <p>I'm {props.name} and I am {props.age}</p>
