d0fc2849f59998527eaa374120b03fd2b5e243c4
Misako Ohata <misa@ohatadesign.com>
2017-12-08 00:38:13 +0900

    update Sec7-95

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
-
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
 class App extends PureComponent {
   constructor( props ) {
     super( props );
@@ -95,7 +95,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App} >
+      <Aux>
         <button onClick={()=> {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -103,10 +103,10 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-      </ WithClass>
+      </ Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
